266661e932ec2b029ba06dcc14a37c17ea6fedc3
jasonmcgetty <jasontmcgetty@yahoo.com>
2025-11-20 20:42:29 -0500

    Filter clean_dataset.csv by track_name+artist instead of just track name

--- clean_dataset.py
+++ clean_dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('dataset.csv')
 
 sorted_df = df.sort_values(by='popularity', ascending=False)
-clean_df = sorted_df.drop_duplicates(subset=['track_name'])
+clean_df = sorted_df.drop_duplicates(subset=['track_name','artists'])
 
 clean_df.to_csv('clean_dataset.csv')
 
